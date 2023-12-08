8208d9d2fc586a5a492fa2547cdcb7a335f09854
Dipesh Shrestha <dev.dipeshstha@gmail.com>
2023-12-08 23:01:30 +0545

    All completed

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,6 +6,62 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+
+const ProjectCard = ({  index, name, description, tags, image, source_code_link }) => {
+  return (
+    <motion.div
+      variants={fadeIn("up","spring",index*0.5, 0.75)}
+    >
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img 
+            src={image} 
+            alt={name} 
+            className="w-full h-full object-cover rounded-2xl" 
+          />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={()=>window.open(source_code_link,"_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img 
+                src={github} 
+                alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div  className="mt-5">
+          <h3 className="text-white font-bold text=[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag)=>(
+            <p
+              key={tag.name}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+
+      </Tilt>
+    </motion.div>
+  )
+}
+
 const Works = () => {
   return (
     <>
@@ -19,8 +75,18 @@ const Works = () => {
           variants={fadeIn("","",0.1,1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora aperiam eos possimus consequuntur minima dolorem dicta id ducimus cupiditate, hic porro dolore perspiciatis nemo quaerat ipsa mollitia harum expedita? Cumque eaque, ut aliquam sunt asperiores consequatur aspernatur est eligendi ducimus dolorem, illum sequi! Nobis optio impedit doloribus doloremque laudantium rerum odit non quam maxime illum repudiandae nemo aliquid, beatae quibusdam fugit nihil aut fugiat eius modi quidem labore quasi! Eum odit animi reprehenderit eos natus esse debitis. Nulla at vitae quo fuga autem corporis quasi fugiat? Beatae sit, molestias consectetur facere cupiditate corporis non quibusdam deserunt est, ut tenetur neque.
+          Following projects showcases my skills and experiences through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problemsm work with different technologies, and manage projects effectively.
         </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index)=>(
+          <ProjectCard  
+            key={`project-${index}`}
+            index={index}
+            {...project}
+          />
+        ))}
       </div>
 
     </>
